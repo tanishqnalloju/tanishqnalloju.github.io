e4a5e22224fc9cae149fd00c33f63f37062347cc
Venkata Sai Tanishq Nalloju <tanishqnrao@gmail.com>
2021-02-26 08:13:15 +0530

    Updated Coming Soon Date message

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@
     * ------------------------------------------------------ */
     const ssFinalCountdown = function() {
 
-        const finalDate = '2020/09/30';
+        const finalDate = '2021/04/15';
 
         $('.counter').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
